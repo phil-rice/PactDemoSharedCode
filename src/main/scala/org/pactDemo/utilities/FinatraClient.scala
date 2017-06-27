f672dda447117d3c07d3dd5438338ace177bc691
Phil <phil.rice@iee.org>
2017-06-27 15:13:59 +0200

    Trying to get client to work from Heroku consumer to Heroku provider

--- src/main/scala/org/pactDemo/utilities/FinatraClient.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraClient.scala
@@ -6,13 +6,14 @@ import com.twitter.util.Future
 import com.twitter.inject.Logging
 
 
-class FinatraClient(hostAndPort: String, fn: String => String) extends Service[Int, String] with com.twitter.inject.Logging{
+class FinatraClient(hostAndPort: String, fn: String => String) extends Service[Int, String] with com.twitter.inject.Logging {
 
   info(s"Trying to start with $hostAndPort")
   val service = Http.newService(hostAndPort)
 
   override def apply(id: Int): Future[String] =
-    service(Request(s"/id/$id")).map(res => fn(res.contentString)).respond { tryT => info(s"FinatraClient $hostAndPort with $id resulted in $tryT")
-    }
+    service(Request(s"/id/$id")).map(res => fn(res.contentString)).
+      respond { tryT => info(s"FinatraClient $hostAndPort with $id resulted in $tryT"); tryT }
+
 
 }
