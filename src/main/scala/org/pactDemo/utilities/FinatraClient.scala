6b08ef1841c8292b597575316adace129b8f53fa
Phil <phil.rice@iee.org>
2017-06-27 09:50:49 +0200

    Setting up binding to heroku port

--- src/main/scala/org/pactDemo/utilities/FinatraClient.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraClient.scala
@@ -5,9 +5,9 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.util.Future
 
 
-class FinatraClient(host: String, defaultPort: Int, fn: String => String) extends Service[Int, String] {
-  val actualPort = Heroku.port(defaultPort)
-  val service = Http.newService(s"$host:$actualPort")
+class FinatraClient(host: String, port: Int, fn: String => String) extends Service[Int, String] {
+
+  val service = Http.newService(s"$host:$port")
 
   override def apply(id: Int): Future[String] =
     service(Request(s"/id/$id")).map(res => fn(res.contentString))
--- src/main/scala/org/pactDemo/utilities/FinatraServer.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraServer.scala
@@ -7,14 +7,17 @@ import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.request.{QueryParam, RouteParam}
 
 /** Admin port is at port +1 */
-class FinatraServer(port: Int, controllers: Controller*) extends HttpServer {
+class FinatraServer(defaultPort: Int, controllers: Controller*) extends HttpServer {
 
+
+  def actualPort = Heroku.port(defaultPort)
   override val modules = Seq()
 
   override val disableAdminHttpServer = true // see https://twitter.github.io/finatra/user-guide/twitter-server/index.html
-  override def defaultHttpPort: Int = port
 
-  override val defaultFinatraHttpPort: String = s":$port"
+  override val defaultFinatraHttpPort: String = s":$actualPort"
+  override def defaultHttpPort: Int = actualPort
+
 
   override def configureHttp(router: HttpRouter): Unit = {
     val raw = router
