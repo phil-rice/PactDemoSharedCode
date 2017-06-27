34434e5f5edbe92ac133825778c8afc62cd941d8
Phil <phil.rice@iee.org>
2017-06-27 09:40:50 +0200

    Modifying readmes and Setting up binding to heroku port

--- src/main/scala/org/pactDemo/utilities/FinatraClient.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraClient.scala
@@ -5,8 +5,9 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.util.Future
 
 
-class FinatraClient(host: String, port: Int, fn: String => String) extends Service[Int, String] {
-  val service = Http.newService(s"$host:$port")
+class FinatraClient(host: String, defaultPort: Int, fn: String => String) extends Service[Int, String] {
+  val actualPort = Heroku.port(defaultPort)
+  val service = Http.newService(s"$host:$actualPort")
 
   override def apply(id: Int): Future[String] =
     service(Request(s"/id/$id")).map(res => fn(res.contentString))
---
+++ src/main/scala/org/pactDemo/utilities/Heroku.scala
@@ -0,0 +1,9 @@
+package org.pactDemo.utilities
+
+object Heroku {
+
+  def port(default: Int): Int = Option(System.getenv("PORT")) match {
+    case Some(p) => p.toInt
+    case + => default
+  }
+}
