f200eb963d21ffbd77568e82f993592ac2aa6d0b
Phil <phil.rice@iee.org>
2017-06-27 16:03:20 +0200

    Adding host header

--- src/main/scala/org/pactDemo/utilities/FinatraClient.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraClient.scala
@@ -6,17 +6,18 @@ import com.twitter.util.Future
 import com.twitter.inject.Logging
 
 
-class FinatraClient(hostAndPort: String, fn: String => String) extends Service[Int, String] with com.twitter.inject.Logging {
+class FinatraClient(host: String, port: Int, fn: String => String) extends Service[Int, String] with com.twitter.inject.Logging {
 
-  info(s"Trying to start with $hostAndPort")
-  println(s"Trying to start with $hostAndPort - println")
-  val service = Http.newService(hostAndPort)
+  info(s"Trying to start with $host:$port")
+  println(s"Trying to start with $host:$port - println")
+  val service = Http.newService(s"$host:$port")
 
   override def apply(id: Int): Future[String] = {
     val request = Request(s"/id/$id")
+    request.host = host
     service(request).map { res =>
       val result = fn(res.contentString)
-      println(s"println inside map for FinatraClient($hostAndPort) request is $request ${res.contentString} => ${result} and the result was $res")
+      println(s"println inside map for FinatraClient($host:$port) request is $request ${res.contentString} => ${result} and the result was $res")
       result
     }
   }
--- src/main/scala/org/pactDemo/utilities/Heroku.scala
+++ src/main/scala/org/pactDemo/utilities/Heroku.scala
@@ -7,8 +7,12 @@ object Heroku {
     case _ => default
   }
 
-  def provider = Option(System.getenv("PORT")) match {
-    case Some(p) => "pact-demo-provider.herokuapp.com:80"// we are in the heroku world
-    case _ => "localhost:9000"
+  def providerHost = Option(System.getenv("PORT")) match {
+    case Some(p) => "pact-demo-provider.herokuapp.com"// we are in the heroku world
+    case _ => "localhost"
+  }
+  def providerPort = Option(System.getenv("PORT")) match {
+    case Some(p) =>80// we are in the heroku world
+    case _ => 9000
   }
 }
