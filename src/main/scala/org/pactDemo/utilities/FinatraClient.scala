ef708a497d15b76dcdf9f6b077d438b99adc238f
Phil <phil.rice@iee.org>
2017-06-27 10:35:16 +0200

    Now linking to heroku for provider when not running locally

--- src/main/scala/org/pactDemo/utilities/FinatraClient.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraClient.scala
@@ -5,9 +5,9 @@ import com.twitter.finagle.{Http, Service}
 import com.twitter.util.Future
 
 
-class FinatraClient(host: String, port: Int, fn: String => String) extends Service[Int, String] {
+class FinatraClient(hostAndPort: String, fn: String => String) extends Service[Int, String] {
 
-  val service = Http.newService(s"$host:$port")
+  val service = Http.newService(hostAndPort)
 
   override def apply(id: Int): Future[String] =
     service(Request(s"/id/$id")).map(res => fn(res.contentString))
