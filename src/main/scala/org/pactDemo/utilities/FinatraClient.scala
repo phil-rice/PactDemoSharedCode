76da1d943703cd96956fd9e5933a3eece3b0684c
Phil <phil.rice@iee.org>
2017-06-27 15:01:10 +0200

    logging client

--- src/main/scala/org/pactDemo/utilities/FinatraClient.scala
+++ src/main/scala/org/pactDemo/utilities/FinatraClient.scala
@@ -3,13 +3,15 @@ package org.pactDemo.utilities
 import com.twitter.finagle.http.Request
 import com.twitter.finagle.{Http, Service}
 import com.twitter.util.Future
+import com.twitter.inject.Logging
 
 
-class FinatraClient(hostAndPort: String, fn: String => String) extends Service[Int, String] {
+class FinatraClient(hostAndPort: String, fn: String => String) extends Service[Int, String] with com.twitter.inject.Logging{
 
   val service = Http.newService(hostAndPort)
 
   override def apply(id: Int): Future[String] =
-    service(Request(s"/id/$id")).map(res => fn(res.contentString))
+    service(Request(s"/id/$id")).map(res => fn(res.contentString)).respond { tryT => info(s"FinatraClient $hostAndPort with $id resulted in $tryT")
+    }
 
 }
